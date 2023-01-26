9fe6b786dc67a57cee760c068e26312bad36ce76
SusandeepShakya <susandeepshakya@gmail.com>
2023-01-26 15:26:25 +0545

    Changese to the Code e

--- src/Component/DatePickerCustom.js
+++ src/Component/DatePickerCustom.js
@@ -32,35 +32,3 @@ function DatePickerCustom(props) {
 
 export default DatePickerCustom;
 
-
-// import { DatePicker } from '@mui/x-date-pickers';
-// import moment from 'moment';
-// import React from 'react'
-// import ReactDatePicker from 'react-datepicker'
-// import "react-datepicker/dist/react-datepicker.css";
-
-
-// const DatePickerCustom = (props) => {
-//     const { label, placeholder, selected, onChange, minDate, maxDate, inputFormat } = props
-//     return (
-//         <>
-//             <span>{label}</span>
-//             {/* <DatePicker
-//                 variant="inline"
-//                 label="Basic example"
-//                 value={selected}
-//                 onChange={onChange}
-//             /> */}
-//             <ReactDatePicker
-//                 selected={selected}
-//                 placeholder={placeholder}
-//                 onChange={onChange}
-//                 minDate={minDate && moment(minDate).toDate()}
-//                 maxDate={maxDate && moment(maxDate).toDate()}
-//                 format={inputFormat || "YYYY-M-D"}
-//             />
-//         </>
-//     )
-// }
-
-// export default DatePickerCustom
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -3,6 +3,6 @@ import moment from "moment/moment";
 export const url_params = {
     page: 1,
     per_page: '',
-    from: '', /* moment().subtract(3, 'months').format('Y-M-d') */
-    to: '', /* moment().format('Y-M-d') */
+    from: '', 
+    to: '',
 }
